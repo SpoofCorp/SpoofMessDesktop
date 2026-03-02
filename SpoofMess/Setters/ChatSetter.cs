f1aefec6dd010968f32f39a3823f05e76c55b994
Pychka <kronossluga@gmail.com>
2026-03-02 22:55:13 +0500

    feat: add model setters

---
+++ SpoofMess/Setters/ChatSetter.cs
@@ -0,0 +1,25 @@
+﻿using CommonObjects.DTO;
+using SpoofMess.Models;
+
+namespace SpoofMess.Setters;
+
+public static class ChatSetter
+{
+    public static Chat Set(this ChatDTO chat) =>
+        new()
+        {
+            Id = chat.Id,
+            Name = chat.Name,
+            UniqueName = chat.UniqueName,
+            Messages = []
+        };
+    public static Chat Set(this ChatUserDTO chat) =>
+        new()
+        {
+            Id = chat.Id,
+            Name = chat.Name,
+            ChatTypeId = chat.ChatTypeId,
+            UniqueName = chat.UniqueName,
+            //Rules = [.. chat.Rules]
+        };
+}
---
+++ SpoofMess/Setters/MessageSetter.cs
@@ -0,0 +1,21 @@
+﻿using CommonObjects.DTO;
+using SpoofMess.Models;
+
+namespace SpoofMess.Setters;
+
+public static class MessageSetter
+{
+    public static MessageModel Set(this MessageDTO message) =>
+        new()
+        {
+            ChatId = message.ChatId,
+            SentAt = message.SendAt,
+            Text = message.Text,
+            UserId = message.UserId,
+            User = new()
+            {
+                Id = message.UserId,
+                Name = message.UserName
+            }
+        };
+}
