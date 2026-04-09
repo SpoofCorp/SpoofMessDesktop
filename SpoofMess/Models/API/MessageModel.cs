8e0650e9dcf04222f9b4b097330928431c19c203
Pychka <kronossluga@gmail.com>
2026-04-09 12:42:00 +0500

    feat: display attachments if message hasn't text & remove padding to addtional views

--- SpoofMess/Models/API/MessageModel.cs
+++ SpoofMess/Models/API/MessageModel.cs
@@ -1,18 +1,18 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using SpoofMess.ServiceRealizations;
 using SpoofMess.ViewModels.FileViewModels;
 using System.Collections.ObjectModel;
 using System.Windows;
 
 namespace SpoofMess.Models;
 
-public partial class MessageModel : ObservableObject
+public partial class MessageModel : ObservableObject, IDisposable
 {
     public Guid Id { get; set; }
     public Guid UserId { get; set; }
     public Guid ChatId { get; set; }
 
+    [NotifyPropertyChangedFor(nameof(PreviewText))]
     [ObservableProperty]
     private string? _text;
     [ObservableProperty]
@@ -21,8 +21,21 @@ public partial class MessageModel : ObservableObject
     private User? user;
     [ObservableProperty]
     private Chat? chat;
+    public string PreviewText
+    {
+        get
+        {
+            if (!string.IsNullOrEmpty(Text))
+                return Text;
+            return string.Join(" ", Attachments.Select(x => x.Icon));
+        }
+    }
+    public MessageModel()
+    {
+        Attachments.CollectionChanged += Attachments_CollectionChanged;
+    }
 
-    public ObservableCollection<FileViewModel> Attachments { get; set; } = [];
+    public ObservableCollection<ObjectViewModel> Attachments { get; set; } = [];
 
     [RelayCommand]
     private void Delete()
@@ -38,5 +51,14 @@ public partial class MessageModel : ObservableObject
     private void Copy()
     {
         Clipboard.SetText(Text ?? "");
+    }
+
+    private void Attachments_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e) =>
+        OnPropertyChanged(nameof(PreviewText));
+
+    public void Dispose()
+    {
+        Attachments.CollectionChanged -= Attachments_CollectionChanged;
+        GC.SuppressFinalize(this);
     }
 }
