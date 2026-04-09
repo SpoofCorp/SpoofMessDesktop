3defcd1ca3cb74df5e699f06a23b562722b0ed30
Pychka <kronossluga@gmail.com>
2026-04-09 12:42:00 +0500

    feat: move registration and authorization controlls to resources, remove tab navigation by textbox and border on PasswordInputBox & InputBox

--- SpoofMess/ViewElements/InputPasswordBox.xaml.cs
+++ SpoofMess/ViewElements/InputPasswordBox.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Text;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace SpoofMess.ViewElements;
@@ -6,11 +7,25 @@ namespace SpoofMess.ViewElements;
 public partial class InputPasswordBox : UserControl
 {
     private readonly static DependencyProperty HolderProperty =
-        DependencyProperty.Register("Holder", typeof(string), typeof(InputTextBox));
+        DependencyProperty.Register(nameof(Holder), typeof(string), typeof(InputPasswordBox));
 
     private readonly static DependencyProperty TextProperty =
-        DependencyProperty.Register("Text", typeof(string), typeof(InputTextBox));
+        DependencyProperty.Register(nameof(Text), typeof(string), typeof(InputPasswordBox));
 
+    private readonly static DependencyProperty WithShowingProperty =
+        DependencyProperty.Register(nameof(WithShowing), typeof(Visibility), typeof(InputPasswordBox));
+
+    private int SelectionStart;
+    readonly StringBuilder builder = new();
+    private TextBlock HolderText = null!;
+    private TextBox Input = null!;
+    public Visibility WithShowing
+    {
+        get => (Visibility)GetValue(WithShowingProperty);
+        set => SetValue(WithShowingProperty, value);
+    }
+
+    private bool show;
 
     public string Holder
     {
@@ -20,6 +35,7 @@ public partial class InputPasswordBox : UserControl
             SetValue(HolderProperty, value);
     }
 
+
     public string Text
     {
         get =>
@@ -27,6 +43,7 @@ public partial class InputPasswordBox : UserControl
         set =>
             SetValue(TextProperty, value);
     }
+
     public InputPasswordBox()
     {
         InitializeComponent();
@@ -35,7 +52,44 @@ public partial class InputPasswordBox : UserControl
     private void InputTextChanged(object sender, TextChangedEventArgs e)
     {
         HolderText.Visibility = string.IsNullOrEmpty(Input.Text)
-            ? Visibility.Visible
-            : Visibility.Collapsed;
+                   ? Visibility.Visible
+                   : Visibility.Collapsed;
+        Input.TextChanged -= InputTextChanged;
+        SelectionStart = Input.SelectionStart;
+        foreach (TextChange item in e.Changes)
+        {
+            if (item.RemovedLength > 0)
+                builder.Remove(item.Offset, item.RemovedLength);
+            if (item.AddedLength > 0)
+                builder.Insert(item.Offset, Input.Text.AsSpan(item.Offset, item.AddedLength));
+        }
+        SetCurrentValue(TextProperty, builder.ToString());
+        if (show)
+            Input.Text = Text;
+        else
+            Input.Text = new string('*', Text.Length);
+        Input.TextChanged += InputTextChanged;
+        Input.SelectionStart = SelectionStart;
+    }
+
+    private void Field_Loaded(object sender, RoutedEventArgs e)
+    {
+        if (Field.Template.FindName(nameof(HolderText), Field) is TextBlock text)
+            HolderText = text;
+        else throw new Exception($"{nameof(HolderText)} doesn't not exists");
+        if (Field.Template.FindName(nameof(Input), Field) is TextBox textBox)
+            Input = textBox;
+        else throw new Exception($"{nameof(Input)} doesn't not exists");
+    }
+
+    private void ToggleButton_Click(object sender, RoutedEventArgs e)
+    {
+        Input.TextChanged -= InputTextChanged;
+        show = !show;
+        if (show)
+            Input.Text = Text;
+        else
+            Input.Text = new string('*', Text.Length);
+        Input.TextChanged += InputTextChanged;
     }
 }
