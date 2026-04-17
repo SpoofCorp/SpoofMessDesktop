bd65233203bef313820962fa65dcb811ea7757e1
Pychka <kronossluga@gmail.com>
2026-04-17 12:42:00 +0500

    feat: add Language & ChatInfo ViewModels

---
+++ SpoofMess/ViewModels/ChatInfoViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommonObjects.Results;
+using CommunityToolkit.Mvvm.Input;
+using SpoofMess.Models;
+using SpoofMess.Services;
+using SpoofMess.Services.Models;
+
+namespace SpoofMess.ViewModels;
+
+public partial class ChatInfoViewModel(IChatAvatarService chatAvatarService, INotificationService notificationService) : AdditionalViewModel
+{
+    private readonly IChatAvatarService _chatAvatarService = chatAvatarService;
+    private readonly INotificationService _notificationService = notificationService;
+    public Chat Chat { get; set; } = null!;
+
+    [RelayCommand]
+    private async Task SetAvatar()
+    {
+        Result result = await _chatAvatarService.Set(Chat);
+        if (!result.Success)
+            _notificationService.ShowToast(new() { Opacity = 1, Text = result.Error ?? result.Message, Type = Enums.NotificationType.Error });
+    }
+}
---
+++ SpoofMess/ViewModels/Settings/LanguageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using SpoofMess.Enums;
+using SpoofMess.Models;
+using SpoofMess.Services;
+
+namespace SpoofMess.ViewModels.Settings;
+
+public partial class LanguageViewModel(UserInfo userInfo, IThemesServices themesService) : AdditionalViewModel
+{
+    private readonly UserInfo _userInfo = userInfo;
+    private readonly IThemesServices _themesService = themesService;
+    public List<Language> Languages { get; set; } = [Language.Ru, Language.En];
+    [ObservableProperty]
+    private Language _language = userInfo.Language;
+
+    partial void OnLanguageChanged(Language value)
+    {
+        _userInfo.Language = value;
+        _themesService.ChangeLanguage(value);
+    }
+}
