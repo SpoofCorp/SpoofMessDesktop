79fdcd97eed4b8410e9f8712e11509a004bbc561
Pychka <kronossluga@gmail.com>
2026-04-28 15:20:42 +0500

    feat: add AddMessage event

--- SpoofMess/ServiceRealizations/EventHandler.cs
+++ SpoofMess/ServiceRealizations/EventHandler.cs
@@ -6,7 +6,16 @@ public class EventHandler
 {
     public delegate void Delete(MessageModel message);
     public delegate void Edit(MessageModel message);
+    public delegate void Add(MessageModel message);
 
-    public static Delete OnDelete = null!;
-    public static Edit OnEdit = null!;
+    public static event Delete OnDelete = null!;
+    public static event Edit OnEdit = null!;
+    public static event Add OnAdd = null!;
+
+    public static void NotifyDelete(MessageModel message) =>
+        OnDelete.Invoke(message);
+    public static void NotifyEdit(MessageModel message) =>
+        OnEdit.Invoke(message);
+    public static void NotifyAdd(MessageModel message) =>
+        OnAdd.Invoke(message);
 }
