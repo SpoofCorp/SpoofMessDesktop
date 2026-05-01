372b56b3f0f8ff5542f945dd9c1e92a75afa2e63
Pychka <kronossluga@gmail.com>
2026-05-01 12:42:42 +0500

    feat: add save user advanced settings

--- SpoofMess/ViewModels/Settings/AdvancedViewModel.cs
+++ SpoofMess/ViewModels/Settings/AdvancedViewModel.cs
@@ -1,8 +1,16 @@
 ﻿using SpoofMess.Models;
+using SpoofMess.Services;
 
 namespace SpoofMess.ViewModels.Settings;
 
-public partial class AdvancedViewModel(UserInfo userInfo) : AdditionalViewModel
+public partial class AdvancedViewModel(UserInfo userInfo, IAuthService authService) : AdditionalViewModel
 {
+    private readonly IAuthService _authService = authService;
     public UserInfo UserInfo { get; set; } = userInfo;
+
+    public override async void OnClose()
+    {
+        await _authService.Save();
+        base.OnClose();
+    }
 }
