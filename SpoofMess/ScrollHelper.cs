00f85f10b6f12c9dc29daf156bf7f24533629f78
Pychka <kronossluga@gmail.com>
2026-05-03 12:32:42 +0500

    feat: create attached property for vertical offset

---
+++ SpoofMess/ScrollHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SpoofMess;
+
+public static class ScrollHelper
+{
+    public static readonly DependencyProperty VerticalOffsetProperty =
+        DependencyProperty.RegisterAttached("VerticalOffset", typeof(double), typeof(ScrollHelper),
+            new PropertyMetadata(0.0, OnVerticalOffsetChanged));
+
+    public static void SetVerticalOffset(DependencyObject target, double value) => target.SetValue(VerticalOffsetProperty, value);
+    public static double GetVerticalOffset(DependencyObject target) => (double)target.GetValue(VerticalOffsetProperty);
+
+    private static void OnVerticalOffsetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is ScrollViewer viewer)
+        {
+            viewer.ScrollToVerticalOffset((double)e.NewValue);
+        }
+    }
+}
