9c15919118313a541b8cbadc069698066bfac5cc
Pychka <kronossluga@gmail.com>
2026-05-03 12:32:42 +0500

    feat: remove selected chat view field & chat messages scroll log

--- SpoofMess/Views/MainView.xaml.cs
+++ SpoofMess/Views/MainView.xaml.cs
@@ -1,6 +1,4 @@
-﻿using SpoofMess.Models;
-using SpoofMess.ViewModels;
-using System.Diagnostics;
+﻿using SpoofMess.ViewModels;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -34,23 +32,5 @@ public partial class MainView : UserControl
     private void ContentPresenter_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
     {
         e.Handled = true;
-    }
-
-    private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
-    {
-        if (ChatsList.SelectedItem is Chat chat && e.VerticalChange > 0)
-        {
-            chat.Position = e.VerticalOffset;
-            Debug.WriteLine(chat.Position);
-            Debug.WriteLine(e.VerticalOffset);
-        }
-    }
-
-    private void ChatsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        if (ChatsList.SelectedItem is Chat chat && MessagesList.Template.FindName("MessageScroll", MessagesList) is ScrollViewer scroll)
-        {
-            scroll.ScrollToVerticalOffset(chat.Position);
-        }
     }
 }
