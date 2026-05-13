296cc1b838e1b350340bef577945951b6132af67
Pychka <kronossluga@gmail.com>
2026-05-13 13:24:21 +0500

    feat: add collectionHelper and DownloadProgress

---
+++ SpoofMess/CollectionHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace SpoofMess;
+
+public static class CollectionHelper
+{
+    extension<T>(ICollection<T> target)
+    {
+        public void ClearAndAddRange(ICollection<T> newCollection)
+        {
+            target.Clear();
+            foreach (T item in newCollection)
+                target.Add(item);
+        }
+    }
+}
---
+++ SpoofMess/DownloadProgress.cs
@@ -0,0 +1,14 @@
+﻿namespace SpoofMess;
+
+public class DownloadProgress
+{
+    public bool IsStarted { get; set; }
+
+    public string Path { get; set; } = string.Empty;
+
+    public event Action<double>? OnChanged;
+
+    public void Raise(double percent) => OnChanged?.Invoke(percent);
+
+    public void Clear() => OnChanged = null;
+}
