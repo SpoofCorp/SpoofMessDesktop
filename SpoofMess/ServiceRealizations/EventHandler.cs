b087eb14a96741c989b8fafe1478c98016d693d9
Pychka <kronossluga@gmail.com>
2026-05-14 13:25:21 +0500

    feat: add UserAvatarUpdated, ChatAvatarUpdated, AddChat and his notification

--- SpoofMess/ServiceRealizations/EventHandler.cs
+++ SpoofMess/ServiceRealizations/EventHandler.cs
@@ -7,15 +7,27 @@ public class EventHandler
     public delegate void Delete(MessageModel message);
     public delegate void Edit(MessageModel message);
     public delegate void Add(MessageModel message);
+    public delegate void AddChat(Chat chat);
+    public delegate void UserAvatarUpdated(FileObject avatar, User user);
+    public delegate void ChatAvatarUpdated(FileObject avatar, Chat chat);
 
     public static event Delete OnDelete = null!;
     public static event Edit OnEdit = null!;
     public static event Add OnAdd = null!;
+    public static event AddChat OnAddChatAdded = null!;
+    public static event UserAvatarUpdated OnUserAvatarUpdated = null!;
+    public static event ChatAvatarUpdated OnChatAvatarUpdated = null!;
 
+    public static void NotifyAddChat(Chat chat) =>
+        OnAddChatAdded?.Invoke(chat);
     public static void NotifyDelete(MessageModel message) =>
-        OnDelete.Invoke(message);
+        OnDelete?.Invoke(message);
     public static void NotifyEdit(MessageModel message) =>
-        OnEdit.Invoke(message);
+        OnEdit?.Invoke(message);
     public static void NotifyAdd(MessageModel message) =>
-        OnAdd.Invoke(message);
+        OnAdd?.Invoke(message);
+    public static void NotifyUserAvatarUpdated(FileObject avatar, User user) =>
+        OnUserAvatarUpdated?.Invoke(avatar, user);
+    public static void NotifyChatAvatarUpdated(FileObject avatar, Chat chat) =>
+        OnChatAvatarUpdated?.Invoke(avatar, chat);
 }
